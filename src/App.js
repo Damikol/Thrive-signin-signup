a682a0d9c960f07e65762f898542c3b9917ab95a
Damikol <vdamilola22@gmail.com>
2023-08-08 03:30:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as Components from './Components';
+import logoImage from './Assets/Logo.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [signIn, toggle] = React.useState(true);
+     return(
+         <Components.Container>
+             <Components.SignUpContainer signinIn={signIn}>
+                 <Components.Form>
+                 <Components.LogoContainer>
+                 <Components.Logo src={logoImage} alt="Logo" />
+                 <Components.LogoText>Thrive</Components.LogoText>
+                 </Components.LogoContainer>
+                     <Components.Title>Create Account</Components.Title>
+                     <Components.Input type='text' placeholder='Name' />
+                     <Components.Input type='email' placeholder='Email' />
+                     <Components.Input type='password' placeholder='Password' />
+                     <Components.Button>Sign Up</Components.Button>
+                     <Components.Anchor href='#'>Need help signing up ?</Components.Anchor>
+                 </Components.Form>
+             </Components.SignUpContainer>
+
+             <Components.SignInContainer signinIn={signIn}>
+                  <Components.Form>
+                  <Components.LogoContainer>
+                  <Components.Logo src={logoImage} alt="Logo" />
+                  <Components.LogoText>Thrive</Components.LogoText>
+                  </Components.LogoContainer>
+                      <Components.Title>Sign in</Components.Title>
+                      <Components.Input type='email' placeholder='Email' />
+                      <Components.Input type='password' placeholder='Password' />
+                      <Components.Button>Sigin In</Components.Button>
+                      <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                  </Components.Form>
+             </Components.SignInContainer>
+
+             <Components.OverlayContainer signinIn={signIn}>
+                 <Components.Overlay signinIn={signIn}>
+
+                 <Components.LeftOverlayPanel signinIn={signIn}>
+                 <Components.LogoContainer>
+                 <Components.Logo src={logoImage} alt="Logo" />
+                 <Components.LogoText>Thrive</Components.LogoText>
+                 </Components.LogoContainer>
+                     <Components.Title>Welcome Back!</Components.Title>
+                     <Components.Paragraph>
+                         To keep you connected with us let's get you signed in with your personal info
+                     </Components.Paragraph>
+                     <Components.GhostButton onClick={() => toggle(true)}>
+                         Sign In
+                     </Components.GhostButton>
+                     <Components.GhostButton onClick={() => toggle(true)}>
+                     <Components.GoogleIcon />Continue with google
+                     </Components.GhostButton>
+                     </Components.LeftOverlayPanel>
+
+                     <Components.RightOverlayPanel signinIn={signIn}>
+                     <Components.LogoContainer>
+                     <Components.Logo src={logoImage} alt="Logo" />
+                     <Components.LogoText>Thrive</Components.LogoText>
+                     </Components.LogoContainer>
+                       <Components.Title>Hello!</Components.Title>
+                       <Components.Paragraph>
+                           Enter your personal details and let's get you started with us
+                       </Components.Paragraph>
+                           <Components.GhostButton onClick={() => toggle(false)}>
+                               Create an account
+                           </Components.GhostButton>
+                           <Components.GhostButton onClick={() => toggle(false)}>
+                           <Components.GoogleIcon />Sign up with Google
+                           </Components.GhostButton>
+                     </Components.RightOverlayPanel>
+
+                 </Components.Overlay>
+             </Components.OverlayContainer>
+
+         </Components.Container>
+     )
 }
 
 export default App;
